d948e4cea68db417e18a78c565eafcda83a65c2f
everzet <ever.zet@gmail.com>
2012-10-29 17:32:04 +0100

    Described simple route collection case

---
+++ spec/Knp/RadBundle/Routing/Loader/ConventionalLoader.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace spec\Knp\RadBundle\Routing\Loader;
+
+use PHPSpec2\ObjectBehavior;
+
+class ConventionalLoader extends ObjectBehavior
+{
+    /**
+     * @param Symfony\Component\Config\FileLocatorInterface $locator
+     * @param Knp\RadBundle\Routing\Loader\YamlParser       $yaml
+     */
+    function let($locator, $yaml)
+    {
+        $this->beConstructedWith($locator, $yaml);
+    }
+
+    function it_should_support_conventional_resources()
+    {
+        $this->supports('', 'conventional')->shouldReturn(true);
+    }
+
+    function it_should_not_support_other_resources()
+    {
+        $this->supports('')->shouldNotReturn(true);
+    }
+
+    function it_should_load_simple_collection_by_conventions($locator, $yaml)
+    {
+        $locator->locate('routing.yml')->willReturn('yaml file');
+        $yaml->parse('yaml file')->willReturn(array(
+            'App:Cheeses' => null
+        ));
+
+        $routes = $this->load('routing.yml');
+
+        $index = $routes->get('app_cheeses_index');
+        $index->getPattern()->shouldReturn('/cheeses/');
+        $index->getDefaults()->shouldReturn(array('_controller' => 'App:Cheeses:index'));
+        $index->getRequirements()->shouldReturn(array('_method' => 'GET'));
+
+        $new = $routes->get('app_cheeses_new');
+        $new->getPattern()->shouldReturn('/cheeses/new');
+        $new->getDefaults()->shouldReturn(array('_controller' => 'App:Cheeses:new'));
+        $new->getRequirements()->shouldReturn(array('_method' => 'GET|POST'));
+
+        $show = $routes->get('app_cheeses_show');
+        $show->getPattern()->shouldReturn('/cheeses/{id}');
+        $show->getDefaults()->shouldReturn(array('_controller' => 'App:Cheeses:show'));
+        $show->getRequirements()->shouldReturn(array('_method' => 'GET', 'id' => '\\d+'));
+
+        $edit = $routes->get('app_cheeses_edit');
+        $edit->getPattern()->shouldReturn('/cheeses/{id}/edit');
+        $edit->getDefaults()->shouldReturn(array('_controller' => 'App:Cheeses:edit'));
+        $edit->getRequirements()->shouldReturn(array('_method' => 'GET|PUT', 'id' => '\\d+'));
+
+        $delete = $routes->get('app_cheeses_delete');
+        $delete->getPattern()->shouldReturn('/cheeses/{id}');
+        $delete->getDefaults()->shouldReturn(array('_controller' => 'App:Cheeses:delete'));
+        $delete->getRequirements()->shouldReturn(array('_method' => 'DELETE', 'id' => '\\d+'));
+
+        $routes->shouldHaveCount(5);
+    }
+}
