3f175e5cfd4ab61f8f90e577aa3070dd02bcae98
Pedro <pierre.plazanet@knplabs.com>
2012-10-29 14:38:31 +0100

    Add an App bundle class

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Knp\RadBundle\KnpRadBundle(),
-            new Knp\RadBundle\AppBundle\Bundle(__DIR__.'/../src/App'),
+            new App\App(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
