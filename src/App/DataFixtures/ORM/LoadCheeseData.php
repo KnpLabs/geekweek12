d38515e254f70fe79f4e88aeab734cfb9ff60069
Pedro <pierre.plazanet@knplabs.com>
2012-10-29 16:02:02 +0100

    Add new cheeses in fixtures

--- src/App/DataFixtures/ORM/LoadCheeseData.php
+++ src/App/DataFixtures/ORM/LoadCheeseData.php
@@ -19,6 +19,56 @@ class LoadCheeseData implements FixtureInterface
         $cheese->setTotalVote(12);
 
         $manager->persist($cheese);
+        
+
+        $cheese = new Cheese();
+        $cheese->setName('Gruyère');
+        $cheese->setRegion('Suisse');
+        $cheese->setMilk('Vache');
+        $cheese->setTotalRating(12*2);
+        $cheese->setTotalVote(12);
+
+        $manager->persist($cheese);
+        
+
+        $cheese = new Cheese();
+        $cheese->setName('Maroilles');
+        $cheese->setRegion('Nord');
+        $cheese->setMilk('Vache');
+        $cheese->setTotalRating(12*2);
+        $cheese->setTotalVote(12);
+
+        $manager->persist($cheese);
+        
+
+        $cheese = new Cheese();
+        $cheese->setName('Munster');
+        $cheese->setRegion('Nord');
+        $cheese->setMilk('Vache');
+        $cheese->setTotalRating(12*3);
+        $cheese->setTotalVote(12);
+
+        $manager->persist($cheese);
+        
+
+        $cheese = new Cheese();
+        $cheese->setName('Ossau-Iraty');
+        $cheese->setRegion('Pyrenées');
+        $cheese->setMilk('Brebis');
+        $cheese->setTotalRating(12*5);
+        $cheese->setTotalVote(12);
+
+        $manager->persist($cheese);
+        
+
+        $cheese = new Cheese();
+        $cheese->setName('Roquefort');
+        $cheese->setRegion('Aveyron');
+        $cheese->setMilk('Brebis');
+        $cheese->setTotalRating(12*1);
+        $cheese->setTotalVote(12);
+
+        $manager->persist($cheese);
 
         $manager->flush();
     }
