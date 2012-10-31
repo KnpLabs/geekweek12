49ea2f1f70251f3e4a098ee5f63b3f68758eb024
everzet <ever.zet@gmail.com>
2012-10-31 15:23:14 +0100

    Implement FormTypeCreator

--- spec/Knp/RadBundle/Form/FormTypeCreator.php
+++ spec/Knp/RadBundle/Form/FormTypeCreator.php
@@ -14,21 +14,53 @@ class FormTypeCreator extends ObjectBehavior
         $this->beConstructedWith($fetcher);
     }
 
-    function it_should_implement_interface()
+    function it_should_implement_form_creator_interface()
     {
         $this->shouldBeAnInstanceOf('Knp\RadBundle\Form\FormCreatorInterface');
     }
 
     /**
-     * @param Knp\Entity\Cheese $cheese
+     * @param stdClass $object
      */
-    function it_should_not_create_a_form_if_form_type_cannot_be_found($cheese, $fetcher)
+    function it_should_return_null_if_there_is_no_form_type($object, $fetcher)
     {
-        $fetcher
-            ->classExists('Knp\Form\CheeseType')
-            ->willReturn(false)
-        ;
+        $fetcher->getClass($object)->willReturn('App\Entity\Potato');
+        $fetcher->classExists('App\Form\PotatoType')->willReturn(false);
+        $fetcher->getParentClass($object)->willReturn(null);
 
-        $this->create($cheese)->shouldBeNull();
+        $this->create($object)->shouldReturn(null);
+    }
+
+    /**
+     * @param stdClass $object
+     * @param stdClass $formType
+     */
+    function it_should_return_form_type_if_there_is_one($object, $fetcher, $formType)
+    {
+        $fetcher->getClass($object)->willReturn('App\Entity\Cheese');
+        $fetcher->classExists('App\Form\CheeseType')->willReturn(true);
+        $fetcher->newInstance(
+            'App\Form\CheeseType', array($object->getWrappedSubject(), array())
+        )->shouldBeCalled()->willReturn($formType);
+
+        $this->create($object)->shouldReturn($formType);
+    }
+
+    /**
+     * @param stdClass $object
+     * @param stdClass $formType
+     */
+    function it_should_return_parent_form_type_if_no_current_found($object, $fetcher, $formType)
+    {
+        $fetcher->getClass($object)->willReturn('App\Entity\Roquefort');
+        $fetcher->classExists('App\Form\RoquefortType')->willReturn(false);
+        $fetcher->getParentClass($object)->willReturn('App\Entity\Cheese');
+        $fetcher->classExists('App\Form\CheeseType')->willReturn(true);
+
+        $fetcher->newInstance(
+            'App\Form\CheeseType', array($object->getWrappedSubject(), array())
+        )->shouldBeCalled()->willReturn($formType);
+
+        $this->create($object)->shouldReturn($formType);
     }
 }
