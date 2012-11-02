0014703b30aaf548229adbad98b03f09ed0d07d0
everzet <ever.zet@gmail.com>
2012-11-02 11:55:36 +0100

    Lowercase repo alias

---
+++ Users/everzet/Sites/geekweek12.dev/src/Knp/RadBundle/Doctrine/EntityRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Knp\RadBundle\Doctrine;
+
+use Doctrine\ORM\BaseEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+
+abstract class EntityRepository extends BaseEntityRepository
+{
+    public function __call($method, array $arguments = array())
+    {
+        if (0 === strpos($method, 'find')) {
+            if (method_exists($builder = 'build'.substr($method, 4))) {
+                $qb = call_user_func_array(array($this, $builder), $arguments);
+
+                return $qb->getQuery()->getResults();
+            }
+        }
+
+        return parent::__call($method, $arguments);
+    }
+
+    protected function build()
+    {
+        return $this->createQueryBuilder($this->getAlias());
+    }
+
+    protected function buildOne($id)
+    {
+        return $this->build()->where($this->getAlias().'.id = '.intval($id));
+    }
+
+    protected function buildAll()
+    {
+        return $this->build();
+    }
+
+    protected function getAlias()
+    {
+        $reflection = new \ReflectionObject($this);
+
+        return strtolower(
+            preg_replace(array('/Repository$/', '/[a-z0-9]/'), '', $reflection->getShortName())
+        );
+    }
+}
